0c6c5ad71de963c16b7542acc4c161ad428f3ec3
Felix-Otieno <omondifelixotieno2@gmail.com>
2024-10-18 11:12:41 +0300

    dart loop

---
+++ dowhile3.dart
@@ -0,0 +1,10 @@
+void main() {
+  int total = 0;
+  int n = 10;
+  int i = 0;
+  do {
+    total = total + i;
+    i++;
+  } while (i <= n);
+  print("Total is $total");
+}
---
+++ while3.dart
@@ -0,0 +1,11 @@
+void main() {
+  int total = 0;
+  int n = 100;
+  int i = 0;
+
+  while (i <= n) {
+    total = total + i;
+    i++;
+  }
+  print("Total is $total");
+}
